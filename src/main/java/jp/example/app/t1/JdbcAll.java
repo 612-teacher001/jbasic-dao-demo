f8068fc959df1febbf109d5b5c9054b57dec134c
villageworks <villageworks@gmail.com>
2025-09-10 04:53:43 +0900

    T1.1 (MLS11): JDBCの利用・全件検索（データベース接続関連オブジェクトの後始末ソース）

--- src/main/java/jp/example/app/t1/JdbcAll.java
+++ src/main/java/jp/example/app/t1/JdbcAll.java
@@ -28,39 +28,45 @@ public class JdbcAll {
 	 * @param args 未使用
 	 */
 	public static void main(String[] args) {
+		// 手順-1. データベース接続関連のオブジェクとの初期化
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
 		try {
-			// 手順-1. 実行するSQLを設定
+			// 手順-2. 実行するSQLを設定
 			String sql = "SELECT * FROM products ORDER BY id";
-			// 手順-2. データベース接続オブジェクトを取得
-			// 2.1 データベース接続情報の取得
+			// 手順-3. データベース接続オブジェクトを取得
+			// 3.1 データベース接続情報の取得
 			DbConfigure configure = new DbConfigure();
 			String url = configure.getUrl();
 			String user = configure.getUser();
 			String pass = configure.getPassword();
-			// 2.2 データベース接続オブジェクトの取得
-			Connection conn = DriverManager.getConnection(url, user, pass);
-			// 手順-3. SQL実行オブジェクトを取得
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			// 手順-4. SQLの実行と結果セットを取得
-			ResultSet rs = pstmt.executeQuery();			// 手順-5. 結果セットから商品リストに変換
-			// 5.1 商品リストの初期化
+			// 3.2 データベース接続オブジェクトの取得
+			conn = DriverManager.getConnection(url, user, pass);
+			// 手順-4. SQL実行オブジェクトを取得
+			pstmt = conn.prepareStatement(sql);
+			// 手順-5. SQLの実行と結果セットを取得
+			rs = pstmt.executeQuery();
+			// 手順-6. 結果セットから商品リストに変換
+			// 6.1 商品リストの初期化
 			List<Product> list = new ArrayList<>();
 			while (rs.next()) {
-				// 5.2 結果セットのすべてのレコードからの商品インスタンスを生成
+				// 6.2 結果セットのすべてのレコードからの商品インスタンスを生成
 				int id = rs.getInt("id");
 				int categoryId = rs.getInt("category_id");
 				String name = rs.getString("name");
 				int price = rs.getInt("price");
 				int quantity = rs.getInt("quantity");
 				Product bean = new Product(id, categoryId, name, price, quantity);
-				// 5.3 商品インスタンスを商品リストに追加
+				// 6.3 商品インスタンスを商品リストに追加
 				list.add(bean);
 			}
-			// 手順-6. 商品リストを表示
-			// 6.1 見出し行の表示
+			// 手順-7. 商品リストを表示
+			// 7.1 見出し行の表示
 			System.out.printf("%-4s\t%-4s\t%-16s\t%-4s\t%-4s\n", 
 							  "商品ID", "カテゴリID", "商品名", "価格", "数量");
-			// 6.2 すべての要素の表示
+			// 7.2 すべての要素の表示
 			for (Product bean : list) {
 				// 6.3 要素の表示
 				System.out.printf("%-4d\t%-4d\t%-16s\t%-4d\t%-4d\n",
@@ -75,6 +81,22 @@ public class JdbcAll {
 		} catch (SQLException e) {
 			// 例外が発生した場合：スタックトレースを表示（必要最低限のエラー情報を表示）
 			e.printStackTrace();
+		} finally {
+			// 手順-8. データベース接続関連のブジェクとのクローズ処理
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				// 例外が発生した場合：スタックトレースを表示（必要最低限のエラー情報を表示）
+				e.printStackTrace();
+			}
 		}
 		
 	}
