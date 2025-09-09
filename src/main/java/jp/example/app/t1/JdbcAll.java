03b350eb094d5f36c416644d6b04086b7bffe86c
villageworks <villageworks@gmail.com>
2025-09-10 06:18:13 +0900

    T1.1 (MLS11): JDBCの利用・全件検索（try-with-resourcesの導入ソース）

--- src/main/java/jp/example/app/t1/JdbcAll.java
+++ src/main/java/jp/example/app/t1/JdbcAll.java
@@ -28,26 +28,21 @@ public class JdbcAll {
 	 * @param args 未使用
 	 */
 	public static void main(String[] args) {
-		// 手順-1. データベース接続関連のオブジェクとの初期化
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+		// 手順-1. 実行するSQLを設定
+		String sql = "SELECT * FROM products ORDER BY id";
+		// 手順-2. データベース接続情報の取得
+		DbConfigure configure = new DbConfigure();
+		String url = configure.getUrl();
+		String user = configure.getUser();
+		String pass = configure.getPassword();
 		
-		try {
-			// 手順-2. 実行するSQLを設定
-			String sql = "SELECT * FROM products ORDER BY id";
-			// 手順-3. データベース接続オブジェクトを取得
-			// 3.1 データベース接続情報の取得
-			DbConfigure configure = new DbConfigure();
-			String url = configure.getUrl();
-			String user = configure.getUser();
-			String pass = configure.getPassword();
-			// 3.2 データベース接続オブジェクトの取得
-			conn = DriverManager.getConnection(url, user, pass);
-			// 手順-4. SQL実行オブジェクトを取得
-			pstmt = conn.prepareStatement(sql);
-			// 手順-5. SQLの実行と結果セットを取得
-			rs = pstmt.executeQuery();
+		try (// 手順-3. データベース接続オブジェクトの取得
+			 Connection conn = DriverManager.getConnection(url, user, pass);
+			 // 手順-4. SQL実行オブジェクトを取得
+			 PreparedStatement pstmt = conn.prepareStatement(sql);
+			 // 手順-5. SQLの実行と結果セットを取得
+			 ResultSet rs = pstmt.executeQuery();
+			) {
 			// 手順-6. 結果セットから商品リストに変換
 			// 6.1 商品リストの初期化
 			List<Product> list = new ArrayList<>();
@@ -81,22 +76,6 @@ public class JdbcAll {
 		} catch (SQLException e) {
 			// 例外が発生した場合：スタックトレースを表示（必要最低限のエラー情報を表示）
 			e.printStackTrace();
-		} finally {
-			// 手順-8. データベース接続関連のオブジェクとのクローズ処理
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				// 例外が発生した場合：スタックトレースを表示（必要最低限のエラー情報を表示）
-				e.printStackTrace();
-			}
 		}
 		
 	}
